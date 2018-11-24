277b88470dc3df266edb35d3e2fceea16a07a3ad
ZYSzys <17367077526@163.com>
2018-11-24 14:48:58 +0800

    chore: extract visitorHelper in rules/no-empty-url.js

--- rules/helper/rule.js
+++ rules/helper/rule.js
@@ -1,6 +1,5 @@
 // 映射关系
 exports.ruleToLevel = rule => {
   return rule === 0 ? 'info' :
-    rule === 1 ? 'warning' :
-      rule === 2 ? 'error' : 'error';
+    rule === 1 ? 'warning' : 'error';
 };

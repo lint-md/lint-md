82b61e8b3d7be27535c32051c7c94de70d28606f
王自强 <814120507@qq.com>
2018-11-25 10:51:42 +0800

    fixed: no-fullwidth-number trigger warning multiply for one line

--- rules/no-fullwidth-number.js
+++ rules/no-fullwidth-number.js
@@ -1,6 +1,7 @@
 const { Plugin } = require('ast-plugin');
+const { isFullwidthNumber, stringAround } = require('./helper/string');
 
-const { isFullwidthNumber } = require('./helper/string');
+const showLength = 10;
 
 /**
  * 无全角数字
@@ -21,16 +22,18 @@ module.exports = class extends Plugin {
        
         const line = ast.node.position.start.line;
         const column = ast.node.position.start.column;
-
+        
         for (let i = 0; i < text.length; i ++) {
           if(isFullwidthNumber(text[i])) {
             const idx = column + i;
-            
+
             this.cfg.throwError({
               line,
-              column: idx, // column 从 1 开始
-              text: 'Number can not be fullwidth',
+              column: idx, 
+              text: stringAround(text, idx - column, showLength)
             });
+            // 每行只检测第一个错误
+            break;
           }
         }
       },

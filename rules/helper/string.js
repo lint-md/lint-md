c02ca824d98549c45ed8f2b1ca61dd22c091d82e
王自强 <814120507@qq.com>
2018-11-25 10:49:42 +0800

    remove unused function isFullwidth, add stringAround to get string fragment easily

--- rules/helper/string.js
+++ rules/helper/string.js
@@ -7,19 +7,23 @@ const isAlphabet = s => {
 };
 
 const isNumber = s => {
-  return /[0-9０-９]/.test(s);
+  return /[0-9]/.test(s);
 };
-
-// 判断是否为全角
-const isFullwidth = s => {
-  return /[\uff00-\uffff]/.test(s);
-}
 
 // 判断是否为全角数字
 const isFullwidthNumber = s => {
   return /[０-９]/.test(s);
 };
 
+/**
+ * 显示从给定位置为原点，一定范围内的字符串片段
+ * @param {String} s         给定字符串
+ * @param {Number} index     原点位置
+ * @param {Number} length    获取字符串长度
+ */
+const stringAround = (s, index, length) => {
+  return s.substr(Math.max(index - Math.floor(length / 2), 0), length);
+}
 
 const stringType = s => {
   return isNumber(s) ? 'N' :
@@ -31,7 +35,7 @@ module.exports = {
   isChinese,
   isAlphabet,
   isNumber,
-  isFullwidth,
   isFullwidthNumber,
-  stringType
+  stringType,
+  stringAround
 }

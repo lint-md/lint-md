f1924452b9a9e1b7b294f1a86bdaaa0ce2b56683
王自强 <814120507@qq.com>
2018-12-07 09:59:08 +0800

    add: ast function astChildrenPos to locate children

--- rules/helper/ast.js
+++ rules/helper/ast.js
@@ -11,6 +11,42 @@ const astToText = ast => {
   return childrenText.join('');
 };
 
+/**
+ * 获取 ast 中的子元素占据的位置
+ * @param {*} ast 
+ */
+const astChildrenPos = ast => {
+  const children = ast.children;
+  if (!children || children.length === 0) {
+    const line = ast.position.start.line;
+    const column = ast.position.start.column;
+    return {
+      start: {
+        line,
+        column
+      },
+      end: {
+        line,
+        column
+      }
+    }
+  }
+
+  const first = children[0];
+  const last = children[children.length - 1];
+  return {
+    start: {
+      line: first.position.start.line,
+      column: first.position.start.column,
+    },
+    end: {
+      line: last.position.end.line,
+      column: last.position.end.column,
+    }
+  }
+}
+
 module.exports = {
   astToText,
+  astChildrenPos
 };

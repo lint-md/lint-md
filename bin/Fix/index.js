d270f39425eb6e4c63859e4d7c8a508f85356b3b
hustcc <i@hust.cc>
2019-02-25 20:46:20 +0800

    fix(cli): ink -> chalk, fixed #35, #33

--- bin/Fix/index.js
+++ bin/Fix/index.js
@@ -1,86 +1,39 @@
-const { h, render, Component, Color } = require('ink');
+const chalk = require('chalk');
 const loadMdFiles = require('../helper/file');
 const fix = require('./fix');
-const string = require('../helper/string');
+
+const log = console.log;
 
 /**
  * Fix 组件
  * @type {module.Fix}
  */
-module.exports = class Fix extends Component {
+module.exports = class Fix {
 
-  constructor(props) {
-    super(props);
+  constructor(files, config) {
+    this.files = files;
+    this.config = config;
 
-    this.state = {
-      files: [], // 处理的文件
-    };
-  }
-
-  componentDidMount() {
     this.start();
   }
 
   async start() {
-    const { files, config, onExit } = this.props;
-
-    const mdFiles = await loadMdFiles(files, config);
+    const mdFiles = await loadMdFiles(this.files, this.config);
 
     for (const file of mdFiles) {
-      const rewrite = await fix(file, config);
-
-      let { files } = this.state;
+      const rewrite = await fix(file, this.config);
 
       // 重新过的，才加进去
       if (rewrite) {
-        files = files.concat(file);
+        this.printFile(file);
       }
-
-      await this.setStateAsync({
-        files,
-      });
     }
 
     // 退出
-    onExit(0);
+    process.exit(0);
   }
 
-  setStateAsync(state) {
-    return new Promise((resolve, reject) => {
-      this.setState(state, () => {
-        resolve();
-      });
-    });
-  };
-
-  renderError(error) {
-    const { start, end, level, text, type } = error;
-    const props = level === 'error' ? { red: true } : { yellow: true };
-
-    const pos = `${start.line}:${start.column}-${end.line}:${end.column}`;
-
-    return h('div', {}, [
-      '  ',
-      h('span', {}, h(Color, { grey: true }, string.rightPad(pos, 16))),
-      '  ',
-      h('span', {}, h(Color, { grey: true }, string.rightPad(`${type}`, 24))),
-      '  ',
-      h(Color, props, text),
-    ]);
-  }
-
-  renderFiles() {
-    const { files } = this.state;
-
-    return h('span', {}, files.map(f => h('div', {}, h(Color, { green: true }, f))));
-  }
-
-  render() {
-    // 渲染错误
-    return h(
-      'div',
-      {},
-      [ this.renderFiles() ]
-    );
+  printFile(file) {
+    log(chalk.green(file));
   }
 };
--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,11 @@
 #!/usr/bin/env node
 
-const { h, render } = require('ink');
 const program = require('commander');
 
 const pkg = require('../package.json');
 const Lint = require('./Lint');
 const Fix = require('./Fix');
 const configure = require('./helper/configure');
-
-let unmount;
-
-const onExit = error => {
-  setTimeout(() => {
-    unmount();
-    process.exit(error);
-  }, 0);
-};
 
 program
   .version(pkg.version, '-v, --version')
@@ -29,9 +19,9 @@ program
     const fix = cmd.fix;
 
     if (fix) {
-      unmount = render(h(Fix, { files, config, onExit }));
+      new Fix(files, config);
     } else {
-      unmount = render(h(Lint, { files, config, onExit }));
+      new Lint(files, config);
     }
   });
 

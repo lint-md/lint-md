80bafa372d5bbdb38f49b77fe3bed23834d3c650
yuzhanglong <yuzl1123@163.com>
2021-03-08 21:48:44 +0800

    chore: beautify the code

--- src/helper/eslint-plugin-lint-md/src/parser.js
+++ src/helper/eslint-plugin-lint-md/src/parser.js
@@ -1,5 +1,15 @@
+/*
+ * File: constants.ts
+ * Description: 面向 md 文件的 eslint 自定义 parser
+ * Created: 2021-3-8 21:40:24
+ * Author: yuzhanglong
+ * Email: yuzl1123@163.com
+ */
+
 module.exports = {
-  parseForESLint(code, config) {
+  // md parser，使用一个定死的 ast，将 code 传给 Literal 的 value，
+  // 这样，我们可以在 自定义的 eslint-rule 中的 Literal() 拿到它，然后查错 / fix it
+  parseForESLint(code) {
     return {
       ast: {
         type: 'Program',
--- src/helper/eslint-plugin-lint-md/src/ruleGenerator.js
+++ src/helper/eslint-plugin-lint-md/src/ruleGenerator.js
@@ -1,19 +1,14 @@
 const fs = require('fs')
 const path = require('path')
+const {getTemplateHeaderComment} = require("./utils");
+const {NOT_SUPPORTED_FIX} = require("./constants");
 
+// paths
 const ruleDir = path.resolve(__dirname, '../../../lint-rules')
 const templatePath = path.resolve(__dirname, './ruleTemplate.js')
 const eslintRuleDir = path.resolve(__dirname, './rules')
 
-const NOT_SUPPORTED_FIX = ['no-long-code']
-
-const HEADERS = `/*
- * IMPORTANT!
- * This file has been automatically generated,
- * in order to update it's content execute "npm run sync-rules"
- */\n\n`
-
-
+// 获取 lint-md 下的所有 rules
 const getTotalRuleNames = () => {
   const ruleFiles = fs.readdirSync(ruleDir)
   return ruleFiles
@@ -21,7 +16,7 @@ const getTotalRuleNames = () => {
     .map(data => data.slice(0, -3))
 }
 
-
+// 生成 eslint rules 文件
 const generateRuleCode = () => {
   const template = fs.readFileSync(templatePath)
   const rules = getTotalRuleNames()
@@ -29,15 +24,18 @@ const generateRuleCode = () => {
 
   // 生成 rules && index.js
   rules.forEach(name => {
+    const comment = getTemplateHeaderComment(name)
+
     const result = template.toString()
       .replace(/\$MD_LINT_RULE_NAME\$/g, name)
       .replace(/\$FIXABLE\$/g, NOT_SUPPORTED_FIX.indexOf(name) >= 0 ? false : '"code"')
 
-    fs.writeFileSync(path.resolve(eslintRuleDir, `${name}.js`), `${HEADERS}${result}`)
+    fs.writeFileSync(path.resolve(eslintRuleDir, `${name}.js`), `${comment}${result}`)
     data += `  '${name}': require('./${name}'),\n`
   })
   data += '}'
-  fs.writeFileSync(path.resolve(eslintRuleDir, 'index.js'), `${HEADERS}${data}`)
+
+  fs.writeFileSync(path.resolve(eslintRuleDir, 'index.js'), `${getTemplateHeaderComment()}${data}`)
 }
 
 generateRuleCode()

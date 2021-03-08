eda0fa28dc0c8fad24edfe7bffedcedc10e2db1c
yuzhanglong <yuzl1123@163.com>
2021-03-08 22:55:40 +0800

    fix: fix eslint show errors when use 'no-long-code' rule

--- src/helper/eslint-plugin-lint-md/src/ruleGenerator.js
+++ src/helper/eslint-plugin-lint-md/src/ruleGenerator.js
@@ -1,20 +1,12 @@
-const fs = require('fs')
 const path = require('path')
+const fs = require("fs");
+const {getTotalRuleNames} = require("./utils");
 const {getTemplateHeaderComment} = require("./utils");
 const {NOT_SUPPORTED_FIX} = require("./constants");
 
 // paths
-const ruleDir = path.resolve(__dirname, '../../../lint-rules')
 const templatePath = path.resolve(__dirname, './ruleTemplate.js')
-const eslintRuleDir = path.resolve(__dirname, './rules')
-
-// 获取 lint-md 下的所有 rules
-const getTotalRuleNames = () => {
-  const ruleFiles = fs.readdirSync(ruleDir)
-  return ruleFiles
-    .filter(res => (res !== 'index.js' && res.endsWith('.js')))
-    .map(data => data.slice(0, -3))
-}
+const eslintRuleDir = path.resolve(__dirname, './rules/noArgsRules')
 
 // 生成 eslint rules 文件
 const generateRuleCode = () => {
@@ -31,11 +23,11 @@ const generateRuleCode = () => {
       .replace(/\$FIXABLE\$/g, NOT_SUPPORTED_FIX.indexOf(name) >= 0 ? false : '"code"')
 
     fs.writeFileSync(path.resolve(eslintRuleDir, `${name}.js`), `${comment}${result}`)
-    data += `  '${name}': require('./${name}'),\n`
+    data += `  '${name}': require('./noArgsRules/${name}'),\n`
   })
   data += '}'
 
-  fs.writeFileSync(path.resolve(eslintRuleDir, 'index.js'), `${getTemplateHeaderComment()}${data}`)
+  fs.writeFileSync(path.resolve(path.resolve(eslintRuleDir, '../'), 'index.js'), `${getTemplateHeaderComment()}${data}`)
 }
 
 generateRuleCode()
--- src/helper/eslint-plugin-lint-md/src/ruleTemplate.js
+++ src/helper/eslint-plugin-lint-md/src/ruleTemplate.js
@@ -1,15 +1,32 @@
 const {lint, getDescription} = require('lint-md')
 const baseFixer = require('lint-md/lib/fix').fix
 
+// 该 rule 是否可以 fix
+const FIXABLE = $FIXABLE$
+
 module.exports = {
   meta: {
     type: 'suggestion',
-    fixable: $FIXABLE$
+    fixable: FIXABLE
   },
   create(context) {
+    // 获取 fixer
+    const getFixer = (node) => {
+      return FIXABLE ? {
+        fix: (fixer) => {
+          const newMarkdown = baseFixer(node.value)
+          return fixer.replaceTextRange(
+            [0, node.value.length - 1],
+            newMarkdown
+          );
+        }
+      } : {}
+    }
+
     return {
       Literal(node) {
         if (node.value) {
+          // 调用 lint 函数
           const errors = lint(node.value);
           const resultErr = errors.filter(e => e.type === '$MD_LINT_RULE_NAME$')
           for (let err of resultErr) {
@@ -20,13 +37,7 @@ module.exports = {
                 start: err.start,
                 end: err.end
               },
-              fix: function (fixer) {
-                const newMarkdown = baseFixer(node.value)
-                return fixer.replaceTextRange(
-                  [0, node.value.length - 1],
-                  newMarkdown
-                );
-              }
+              ...getFixer(node)
             })
           }
         }

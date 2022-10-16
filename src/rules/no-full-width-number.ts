bedf40a9dac60ff0b6d63d2200346396a767c389
yuzhanglong <loveyzl1123@gmail.com>
2022-10-17 00:56:58 +0800

    feat: finish no full width number

---
+++ src/rules/no-full-width-number.ts
@@ -0,0 +1,82 @@
+import { MarkdownNode } from '@lint-md/parser';
+import { LintMdRule } from '../types';
+
+const FULL_WIDTH_NUMBER_REPLACEMENT_MAP = {
+  '１': 1,
+  '２': 2,
+  '３': 3,
+  '４': 4,
+  '５': 5,
+  '６': 6,
+  '７': 7,
+  '８': 8,
+  '９': 9,
+  '０': 0
+};
+
+/**
+ * 从字符串中找出所有全角数字
+ * @param s
+ * @returns {Array}
+ */
+const findAllFullWidthNumbers = (s: string) => {
+  const re = new RegExp('[０-９]+', 'g');
+  const r: { number: string, index: number }[] = [];
+
+  // 循环找出所有的数字
+  while (true) {
+    const matched = re.exec(s);
+
+    if (matched) {
+      r.push({
+        number: matched[0],
+        index: matched.index
+      });
+    } else {
+      break;
+    }
+  }
+  return r;
+};
+
+const noFullWidthNumber: LintMdRule = {
+  create: (context) => {
+    return {
+      text: (node: MarkdownNode & {
+        value: string
+      }) => {
+        const text = node.value;
+        findAllFullWidthNumbers(text).forEach(res => {
+          const { index, number } = res;
+          const { line: startLine, column: startColumn, offset: startOffset } = node.position.start;
+
+          const startPos = startColumn + index;
+          const endPos = startColumn + index + number.length;
+
+          context.report({
+            loc: {
+              start: {
+                line: startLine,
+                column: startPos
+              },
+              end: {
+                line: startLine,
+                column: endPos
+              }
+            },
+            message: '[lint-md] 不能用全角数字，请使用半角数字',
+            fix: (fixer) => {
+              // １０００ => '1000'
+              const replacement = number.split('').map(c => `${FULL_WIDTH_NUMBER_REPLACEMENT_MAP[c]}`).join('');
+              return fixer.replaceTextRange([startOffset + index, startOffset + index + number.length], replacement);
+            }
+          });
+        });
+      }
+    };
+  }
+};
+
+export default noFullWidthNumber;
+
+

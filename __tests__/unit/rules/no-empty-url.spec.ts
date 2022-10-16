5c6904d676316fa054e0a3df92e9462c1a28b95f
怀策策 <loveyzl1123@gmail.com>
2022-10-16 12:35:04 +0800

    feat: finish no empty url (#83)
    * feat: finish no empty url
    
    * test: add test case

---
+++ __tests__/unit/rules/no-empty-url.spec.ts
@@ -0,0 +1,26 @@
+import { createFixer } from '../../utils/test-utils';
+import noEmptyURL from '../../../src/rules/no-empty-url';
+
+const fixer = createFixer([{
+  rule: noEmptyURL
+}]);
+
+describe('test no-empty-url', () => {
+  test('fix applied (for link)', () => {
+    const md = '参考资料：[JavaScript 高级程序设计]()';
+    const { lintResult } = fixer(md);
+    expect(lintResult.ruleManager.getReportData().length).toStrictEqual(1);
+  });
+
+  test('fix applied (for image)', () => {
+    const md = '快看看：![JavaScript 高级程序设计]()';
+    const { lintResult } = fixer(md);
+    expect(lintResult.ruleManager.getReportData().length).toStrictEqual(1);
+  });
+
+  test('fix applied (链接全部为空格)', () => {
+    const md = '快看看：![JavaScript 高级程序设计](    )';
+    const { lintResult } = fixer(md);
+    expect(lintResult.ruleManager.getReportData().length).toStrictEqual(1);
+  });
+});

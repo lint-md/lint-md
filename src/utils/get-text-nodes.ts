4f5ccba5baed78134d48e90aee6b3ce140d8bf84
怀策策 <loveyzl1123@gmail.com>
2022-10-18 18:18:32 +0800

    feat: finish no-trailing-punctuation (#91)

---
+++ src/utils/get-text-nodes.ts
@@ -0,0 +1,30 @@
+/**
+ * 给定一个 Markdown 节点，递归寻找所有文本节点
+ * @author YuZhanglong <loveyzl1123@gmail.com>
+ */
+import { MarkdownNode } from '@lint-md/parser';
+
+type MarkdownTextNode = MarkdownNode & {
+  value?: string
+}
+
+
+export const getTextNodes = (node: MarkdownTextNode) => {
+  const textNodes: MarkdownTextNode[] = [];
+
+  // 有 value 字段被认为是 textNode，结束递归
+  if (node.type === 'text') {
+    textNodes.push(node);
+    return textNodes;
+  }
+
+  if (node.children) {
+    const childNodes = node.children.reduce((prev, current) => {
+      return prev.concat(getTextNodes(current as MarkdownTextNode));
+    }, []);
+    textNodes.push(...childNodes);
+  }
+
+
+  return textNodes;
+};

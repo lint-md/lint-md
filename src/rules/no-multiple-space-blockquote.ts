2cabc45dca091f9b5a2f92a413a94dd5c27ba923
怀策策 <loveyzl1123@gmail.com>
2022-10-18 13:41:31 +0800

    feat: finish no-multiple-space-blockquote (#90)

---
+++ src/rules/no-multiple-space-blockquote.ts
@@ -0,0 +1,38 @@
+import { MarkdownNode } from '@lint-md/parser';
+import { LintMdRule } from '../types';
+
+type MarkdownCodeNode = MarkdownNode & {
+  value: string
+}
+
+const noMultipleSpaceBlockquote: LintMdRule = {
+  create: (context) => {
+    return {
+      blockquote: (node: MarkdownCodeNode) => {
+        const blockQuoteColumn = node.position.start.column;
+        const firstChild = node.children[0];
+        if (firstChild) {
+          const blockQuoteFirstChildColumn = firstChild.position.start.column;
+          const deltaColumn = blockQuoteFirstChildColumn - blockQuoteColumn;
+          if (deltaColumn !== 2) {
+            const fixStartRange = node.position.start.offset + 1;
+            const fixEndRange = deltaColumn > 0 ? node.position.start.offset + deltaColumn : fixStartRange + 1;
+
+            context.report({
+              loc: node.position,
+              message: 'blockquote should not have multiple space',
+              fix: (fixer) => {
+                return fixer.replaceTextRange(
+                  [fixStartRange, fixEndRange],
+                  ' '
+                );
+              }
+            });
+          }
+        }
+      }
+    };
+  }
+};
+
+export default noMultipleSpaceBlockquote;

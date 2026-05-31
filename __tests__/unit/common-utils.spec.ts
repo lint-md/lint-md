b4e6cacc3d0fc3b8c02d3af2911f8ee72ed8a5f7
luojiyin <luojiyin@hotmail.com>
2026-05-31 23:44:18 +0800

    test: add unit tests for char-helper functions
    Cover isNumberCharacter, isChineseCharacter, isEnglishCharacter.

--- __tests__/unit/common-utils.spec.ts
+++ __tests__/unit/common-utils.spec.ts
@@ -1,9 +1,25 @@
 import { parseMd } from '@lint-md/parser';
+import {
+  isChineseCharacter,
+  isEnglishCharacter,
+  isNumberCharacter
+} from '../../src/utils/char-helper';
 import { getTextNodes } from '../../src/utils/get-text-nodes';
 
 describe('test common utils', () => {
   test('test get text node', () => {
     const res = getTextNodes(parseMd('这就是 ~~删除线~~ ![12312313](213213) **啦啦啦**<div>~~123123~~!<a>测试测试</a></div> [123123123!!](12312313)'));
     expect(res).toMatchSnapshot();
+  });
+
+  test('test char helper', () => {
+    expect(isNumberCharacter('1')).toBe(true);
+    expect(isNumberCharacter('a')).toBe(false);
+
+    expect(isChineseCharacter('中')).toBe(true);
+    expect(isChineseCharacter('1')).toBe(false);
+
+    expect(isEnglishCharacter('a')).toBe(true);
+    expect(isEnglishCharacter('中')).toBe(false);
   });
 });

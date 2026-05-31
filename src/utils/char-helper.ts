47b237c43a668619c46b5fd0340cb8f3d002ce17
luojiyin <luojiyin@hotmail.com>
2026-05-31 23:28:35 +0800

    perf: hoist regex literals to module-level constants
    Three regex patterns in isNumberCharacter/isChineseCharacter/
    isEnglishCharacter were re-created on every invocation. Extract
    them to module-level constants to avoid repeated RegExp allocation.

--- src/utils/char-helper.ts
+++ src/utils/char-helper.ts
@@ -1,11 +1,7 @@
-export const isNumberCharacter = (value: string) => {
-  return /^[0-9]$/.test(value);
-};
+const NUMBER_RE = /^[0-9]$/;
+const CHINESE_RE = /^[\u4E00-\u9FA5]$/;
+const ENGLISH_RE = /^[a-zA-Z]$/;
 
-export const isChineseCharacter = (value: string) => {
-  return /^[\u4E00-\u9FA5]$/.test(value);
-};
-
-export const isEnglishCharacter = (value: string) => {
-  return /^[a-zA-Z]$/.test(value);
-};
+export const isNumberCharacter = (value: string) => NUMBER_RE.test(value);
+export const isChineseCharacter = (value: string) => CHINESE_RE.test(value);
+export const isEnglishCharacter = (value: string) => ENGLISH_RE.test(value);

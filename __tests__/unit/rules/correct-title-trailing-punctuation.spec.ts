74ff460844730f4b38c310a740c54ab409352b3d
luojiyin <luojiyin@hotmail.com>
2026-06-03 00:23:09 +0800

    test: add inline code edge case tests for correct-title-trailing-punctuation

--- __tests__/unit/rules/correct-title-trailing-punctuation.spec.ts
+++ __tests__/unit/rules/correct-title-trailing-punctuation.spec.ts
@@ -37,4 +37,18 @@ describe('test correct-title-trailing-punctuation', () => {
     expect(lintResult.ruleManager.getReportData().length).toStrictEqual(1);
     expect(fixedResult?.result).toStrictEqual('# 这就是 ~~删除线~~ ![12312313](213213) **啦啦啦**<div>~~123123~~!<a>测试测试</a></div> [123123123](12312313)');
   });
+
+  test('do not remove punctuation from trailing inline code', () => {
+    const md = '#### 强调符号 `#` 和 `~`';
+    const { fixedResult, lintResult } = fixer(md);
+    expect(lintResult.ruleManager.getReportData().length).toStrictEqual(0);
+    expect(fixedResult?.result).toStrictEqual(md);
+  });
+
+  test('still report trailing punctuation before trailing inline code', () => {
+    const md = '# 这是一个错误标题。`code`';
+    const { fixedResult, lintResult } = fixer(md);
+    expect(lintResult.ruleManager.getReportData().length).toStrictEqual(1);
+    expect(fixedResult?.result).toStrictEqual('# 这是一个错误标题`code`');
+  });
 });

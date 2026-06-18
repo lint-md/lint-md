47ad50e2a13239c51bb32d4bcd624c7373ced037
luojiyin <luojiyin@hotmail.com>
2026-06-18 22:59:57 +0800

    fix(common): change isNode parameter from any to unknown with type guard

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,8 +1,10 @@
+import type { MarkdownNode } from '@lint-md/parser';
+
 /**
  * 判断是否为一个合法的 ast 节点
  *
  * @date 2021-12-12 22:01:31
  */
-export const isNode = (x: any) => {
-  return x !== null && typeof x === 'object' && typeof x.type === 'string';
+export const isNode = (x: unknown): x is MarkdownNode => {
+  return x !== null && typeof x === 'object' && typeof (x as Record<string, unknown>).type === 'string';
 };

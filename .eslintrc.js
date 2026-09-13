1fd8b25ac826f8b61b3b24e03c59614d64739152
luojiyin <luojiyin@hotmail.com>
2026-09-13 22:18:05 +0800

    chore(typescript-eslint): upgrade to v8

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,22 @@
 module.exports = {
   extends: '@attachments/eslint-config',
+  rules: {
+    // typescript-eslint v8 removed these formatting rules from the plugin.
+    '@typescript-eslint/brace-style': 'off',
+    '@typescript-eslint/comma-spacing': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/keyword-spacing': 'off',
+    '@typescript-eslint/lines-between-class-members': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-extra-parens': 'off',
+    '@typescript-eslint/object-curly-spacing': 'off',
+    '@typescript-eslint/quotes': 'off',
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/space-before-blocks': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/space-infix-ops': 'off',
+    '@typescript-eslint/type-annotation-spacing': 'off'
+  },
   overrides: [
     {
       files: ['scripts/**/*.mjs', '__tests__/utils/**/*.ts'],
